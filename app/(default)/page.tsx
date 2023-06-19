107410c542f4d795ce43b95ffc83eaebec855f3e
ColeInc <chm@farmside.co.nz>
2023-06-20 08:36:45 +1200

    add feature photos, edit hero text, remove video, update favicon

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,22 +1,22 @@
 export const metadata = {
-  title: 'Home - Open PRO',
-  description: 'Page description',
-}
+    title: "Nardium",
+    description: "Explore and Navigate your Google Docs with Ease! 🔍",
+};
 
-import Hero from '@/components/hero'
-import Features from '@/components/features'
-import Newsletter from '@/components/newsletter'
-import Zigzag from '@/components/zigzag'
-import Testimonials from '@/components/testimonials'
+import Hero from "@/components/hero";
+import Features from "@/components/features";
+import Newsletter from "@/components/newsletter";
+import Zigzag from "@/components/zigzag";
+import Testimonials from "@/components/testimonials";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Features />
-      <Zigzag />
-      <Testimonials />
-      <Newsletter />
-    </>
-  )
+    return (
+        <>
+            <Hero />
+            <Features />
+            <Zigzag />
+            <Testimonials />
+            <Newsletter />
+        </>
+    );
 }
