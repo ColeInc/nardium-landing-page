5db532b037c6d25d2ffc99fa2346980e5ed50d0a
ColeInc <chm@farmside.co.nz>
2023-06-22 09:01:17 +1200

    remove reset-password temporarily

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <div className="flex flex-col min-h-screen overflow-hidden">
                     <Header />
                     {children}
-                    <Banner />
+                    {/* <Banner /> */}
                 </div>
             </body>
         </html>
