b1337d2afb9d294083334e74f8da8b50d98e1734
ColeInc <chm@farmside.co.nz>
2023-06-22 08:43:06 +1200

    adding favicon correctly

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,9 @@
 export const metadata = {
     title: "Nardium",
     description: "Explore and Navigate your Google Docs with Ease! 🔍",
+    icons: {
+        icon: "/images/favicon.ico",
+    },
 };
 
 import Hero from "@/components/hero";
