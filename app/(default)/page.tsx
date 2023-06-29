a2ef0b9921dd43caa96b0e32b40739d21d1fd36d
ColeInc <chm@farmside.co.nz>
2023-06-29 14:13:01 +1200

    disable unused page sections for now

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
             <ProductImage />
             <Features />
             <Zigzag />
-            <Testimonials />
-            <Newsletter />
+            {/* <Testimonials /> */}
+            {/* <Newsletter /> */}
         </>
     );
 }
