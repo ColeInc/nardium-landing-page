6750a78475a1eb22bfc95fb06cec2e780e85d1cd
ColeInc <chm@farmside.co.nz>
2023-07-03 07:50:58 +1200

    finally got favicon working

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,39 +1,33 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-import PageIllustration from '@/components/page-illustration'
-import Footer from '@/components/ui/footer'
+import PageIllustration from "@/components/page-illustration";
+import Footer from "@/components/ui/footer";
+import { Metadata } from "next";
 
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {  
+export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+        AOS.init({
+            once: true,
+            disable: "phone",
+            duration: 600,
+            easing: "ease-out-sine",
+        });
+    });
 
-  useEffect(() => {
-    AOS.init({
-      once: true,
-      disable: 'phone',
-      duration: 600,
-      easing: 'ease-out-sine',
-    })
-  })
+    return (
+        <>
+            <main className="grow">
+                <PageIllustration />
 
-  return (
-    <>
-      <main className="grow">
+                {children}
+            </main>
 
-        <PageIllustration />
-
-        {children}
-
-      </main>
-
-      <Footer />
-    </>
-  )
+            <Footer />
+        </>
+    );
 }
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,10 +1,22 @@
-export const metadata = {
-    title: "Nardium",
-    description: "Explore and Navigate your Google Docs with Ease! 🔍",
-    icons: {
-        icon: "/favicon.ico",
-    },
-};
+// export const metadata = {
+//     title: "Nardium",
+//     description: "Explore and Navigate your Google Docs with Ease! 🔍",
+//     icons: {
+//         icon: "/favicon.ico",
+//     },
+// };
+
+// export const metadata: Metadata = {
+//     title: "Nardium",
+//     description: "Explore and Navigate your Google Docs with Ease! 🔍",
+//     icons: {
+//         icon: {
+//             url: "/favicon.ico",
+//             type: "image/ico",
+//         },
+//         shortcut: { url: "/favicon.ico", type: "image/ico" },
+//     },
+// };
 
 import Hero from "@/components/hero";
 import Features from "@/components/features";
@@ -13,6 +25,7 @@ import Zigzag from "@/components/zigzag";
 import Testimonials from "@/components/testimonials";
 import ProductImage from "@/components/product-image";
 import CallToAction from "@/components/call-to-action";
+import { Metadata } from "next";
 
 export default function Home() {
     return (
