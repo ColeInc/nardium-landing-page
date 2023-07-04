5e3ef2fdf9e28f32e192364462bcf04a252b26c6
ColeInc <chm@farmside.co.nz>
2023-07-05 08:58:47 +1200

    feature request email functionality working! also page styling done

--- app/(auth)/feature-requests/page.tsx
+++ app/(auth)/feature-requests/page.tsx
@@ -1,12 +1,20 @@
 "use client";
 
-import Link from "next/link";
-import Image from "next/image";
-import GoogleLogo from "@/public/images/google-logo.svg";
-import { ChangeEvent, ChangeEventHandler, FormEvent, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { ChangeEvent, FormEvent, LegacyRef, useEffect, useRef, useState } from "react";
+
+const emailjsPublicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY ?? "";
+const emailjsTemplateKey = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_KEY ?? "";
+const emailjsServiceKey = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_KEY ?? "";
+
+console.log("fetched deeze", emailjsPublicKey, emailjsTemplateKey, emailjsServiceKey);
 
 export default function FeatureRequests() {
     const [featureRequest, setFeatureRequest] = useState("");
+    const emailRef = useRef<HTMLInputElement>(null);
+    const bodyRef = useRef<HTMLTextAreaElement>(null);
+
+    useEffect(() => emailjs.init(emailjsPublicKey), []);
 
     const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
         setFeatureRequest(e.target.value);
@@ -14,12 +22,28 @@ export default function FeatureRequests() {
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const email = "nardiumapp@gmail.com";
-        const subject = "Nardium - Feature Request";
-        const body = `Feature Request: ${featureRequest}`;
-        window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(
-            body
-        )}`;
+
+        if (!bodyRef.current?.value) {
+            return;
+        }
+
+        // fire email to support letting them know of new feature request!
+        emailjs.send(
+            emailjsServiceKey,
+            emailjsTemplateKey,
+            {
+                sender: emailRef.current?.value,
+                message: bodyRef.current?.value,
+            },
+            emailjsPublicKey
+        );
+
+        if (emailRef.current) {
+            emailRef.current.value = "";
+        }
+        if (bodyRef.current) {
+            bodyRef.current.value = "";
+        }
     };
 
     return (
@@ -32,24 +56,24 @@ export default function FeatureRequests() {
                     </div>
 
                     {/* Form */}
-                    <div>
-                        <form onSubmit={handleSubmit} className="flex justify-center">
-                            {/* <input
-                                type="text"
-                                placeholder="What would you like to be added?"
-                                value={featureRequest}
-                                onChange={handleInputChange}
-                                className="max-w-xl px-4 py-2 border border-gray-300 rounded-l-md focus:outline-none focus:ring-2 focus:ring-blue-500 flex-grow"
-                                /> */}
+                    <div className="flex flex-col justify-center items-center">
+                        <form onSubmit={handleSubmit} className="flex flex-col items-center max-w-[600px] w-full">
+                            <input
+                                ref={emailRef}
+                                type="email"
+                                className="w-full min-h-[15px] px-4 py-2 mb-2 border border-gray-300 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                placeholder="Your email"
+                            />
                             <textarea
                                 placeholder="What would you like to be added?"
                                 value={featureRequest}
                                 onChange={handleInputChange}
-                                className="min-w-[700px] min-h-[120px] px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                className="w-full min-h-[160px] px-4 py-2 border border-gray-300 rounded-lg resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                ref={bodyRef}
                             />
                             <button
                                 type="submit"
-                                className="px-4 py-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600 focus:outline-none"
+                                className=" w-[180px] px-4 py-2 mt-2 self-end bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
                             >
                                 Request
                             </button>
