90c31ca014d409cea3049781ef96c4eb03a30ae2
ColeInc <chm@farmside.co.nz>
2023-07-12 08:13:23 +1200

    add env vars to next config

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@
 
 const nextConfig = {
     images: { unoptimized: true },
+    basePath: "/nardium-landing-page",
+    output: "export",
+    env: {
+        NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: "qt3ZWXcX3l_wiAFhi",
+        NEXT_PUBLIC_EMAILJS_TEMPLATE_KEY: "template_xpkcxon",
+        NEXT_PUBLIC_EMAILJS_SERVICE_KEY: "service_02p6hzw",
+        NEXT_PUBLIC_GOOGLE_ANALYTICS_MEASUREMENT_ID: "G-ZX4L05HKN9",
+    },
     // experimental: {
     //     appDir: true,
     // },
-    basePath: "/nardium-landing-page",
-    output: "export",
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
